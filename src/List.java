7bad2537ba81a00d7e95ec9315424e684ff54983
Michael Dorst <code@mikefdorst.com>
2016-03-02 17:03:35 -0800

    Use spaces instead of tabs

--- src/List.java
+++ src/List.java
@@ -1,42 +1,41 @@
 
 abstract class List {
 
-	ListItem head = null;
-	ListItem tail = null;
-	int count;
-	
-	public void init(){
-		
-	}
-	
-	public void insert(ListItem newEntry){
-		
-		ListItem nextEntry = new ListItem(newEntry);
-		if(head == null){
-			head = nextEntry;
-			tail = head;
-		}else
-			
-		count++;
-	}
-	
-	public void delete(){
-		
-	}
-	
-	public ListItem search(){
-		
-		return found;
-	}
+    ListItem head = null;
+    ListItem tail = null;
+    int count;
+
+    public void init(){
+
+    }
+
+    public void insert(ListItem newEntry){
+
+        ListItem nextEntry = new ListItem(newEntry);
+        if(head == null){
+            head = nextEntry;
+            tail = head;
+        }else
+
+            count++;
+    }
+
+    public void delete(){
+
+    }
+
+    public ListItem search(){
+
+        return found;
+    }
     public boolean isEmpty(){
-    	if(head == null)
-    	  return true;
-    	else
-    	  return false;
+        if(head == null)
+            return true;
+        else
+            return false;
     }
     public boolean isFull(){
-    	
-    }
-	public void sort();
 
+    }
+    public void sort();
 }
