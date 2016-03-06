6427a10183520baa1a5b217a16a99230895c5c5d
DarkOhms <luke_martin45@yahoo.com>
2016-03-05 23:24:10 -0800

    More insert.  Also changed from abstract
    going to do ordered and unordered lists

--- src/List.java
+++ src/List.java
@@ -1,5 +1,5 @@
 
-abstract class List <T> {
+class List <T extends Comparable> {
 
     ListItem<T> head = null;
     ListItem<T> tail = null;
@@ -7,12 +7,26 @@ abstract class List <T> {
 
     public void insert(ListItem newEntry){
 
-        ListItem<T> nextEntry = new ListItem<>(newEntry);
-        if (head == null) {
+        ListItem<T> nextEntry = new ListItem<T>(newEntry);
+        if(head == null) {
             head = nextEntry;
             tail = head;
-        } else
-            count++;
+        }else
+        if(nextEntry.data.compareTo(head.data)<0){
+            nextEntry.next = head;
+            head = nextEntry;
+        }else
+        if(nextEntry.data.compareTo(tail.data)>0){
+        	 tail.next = nextEntry;
+             tail = nextEntry;
+        }else{
+        	ListItem temp = head;
+        	 while (temp.next.data.compareTo(nextEntry.data) < 0)
+                 temp = temp.next;
+             nextEntry.next = temp.next;
+             temp.next = nextEntry;
+        }
+        	count++;
     }
 
     public void delete(){
@@ -30,7 +44,7 @@ abstract class List <T> {
     }
     public boolean isFull(){
        
-    	ListItem<T> test = new ListItem<T>;
+    	ListItem<T> test = new ListItem<T>();
         if(test == null)
         	return true;
         else
