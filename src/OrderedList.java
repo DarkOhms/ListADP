c73c260a723ad09fa891ae0cb37a192cb7693d57
DarkOhms <luke_martin45@yahoo.com>
2016-03-09 20:35:12 -0800

    fixes
    asdfjv

--- src/OrderedList.java
+++ src/OrderedList.java
@@ -1,6 +1,6 @@
 
 public class OrderedList<T extends Comparable<T>> extends List<T> {
-	
+
 	public void insert(T newEntry){
 
 	        ListItem<T> nextEntry = new ListItem<T>(newEntry);
@@ -10,21 +10,22 @@ public class OrderedList<T extends Comparable<T>> extends List<T> {
 	        }else
 	        if(nextEntry.data.compareTo(head.data)<0){
 	            nextEntry.next = head;
+	            head.prev = nextEntry;
 	            head = nextEntry;
 	        }else
 	        if(nextEntry.data.compareTo(tail.data)>0){
 	        	nextEntry.prev = tail; 
 	        	tail.next = nextEntry;
-	             tail = nextEntry;
+	            tail = nextEntry;
 	        }else{
 	        	ListItem<T> temp = head;
 	        	 while (temp.next.data.compareTo(nextEntry.data) < 0)
 	               temp = temp.next;
 	        	 
 	        	 
-	               nextEntry.next = temp.next;
-	               nextEntry.prev = temp.prev;
-	               temp.next = nextEntry;
+	        	 nextEntry.next = temp.next;
+	        	 temp.next = nextEntry;
+	             nextEntry.prev = temp.prev;
 	        }
 	        	count++;
 	    }
